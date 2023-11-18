22d7e4869e4372c111b0cf4d9d455d01f17bc47f
AheionokDmytro <151346845+AheionokDmytro@users.noreply.github.com>
2023-11-18 09:29:49 +0200

    Add files via upload

---
+++ UnitTest.cpp
@@ -0,0 +1,112 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "D:\university\ipz\IPZ_exercises\IPZ_exercises\lab_2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(CalculateTests)
+	{
+	public:
+
+		TEST_METHOD(calculate_get4and3_6returned)
+		{
+			double x = 4;
+			int n = 3;
+			double expected = 10;
+
+			double actual = calculate(x, n);
+
+			Assert::AreEqual(expected, actual);
+		}
+	public:
+
+		TEST_METHOD(calculate_getneg5and2_neg9returned)
+		{
+			double x = -5;
+			int n = 2;
+			double expected = -9;
+
+			double actual = calculate(x, n);
+
+			Assert::AreEqual(expected, actual);
+		}
+	};
+
+	TEST_CLASS(checkValidParams_Test)
+	{
+	public:
+
+		TEST_METHOD(checkValidParams_getneg10_10_2_10_exceptionNotThrown)
+		{
+			double a = -10;
+			double b = 10;
+			double h = 2;
+			int n = 10;
+
+			try {
+				checkValidParams(a, b, h, n);
+				Assert::IsTrue(true);
+			}
+			catch (...)
+			{
+				Assert::Fail();
+			}
+		}
+	public:
+
+		TEST_METHOD(checkValidParams_get10_neg10_2_10_exceptionThrown)
+		{
+			double a = 10;
+			double b = -10;
+			double h = 2;
+			int n = 10;
+
+			try {
+				checkValidParams(a, b, h, n);
+				Assert::Fail();
+			}
+			catch (...)
+			{
+				Assert::IsTrue(true);
+			}
+		}
+	public:
+
+		TEST_METHOD(checkValidParams_getneg10_10_neg2_10_exceptionThrown)
+		{
+			double a = -10;
+			double b = 10;
+			double h = -2;
+			int n = 10;
+
+			try {
+				checkValidParams(a, b, h, n);
+				Assert::IsTrue(true);
+			}
+			catch (...)
+			{
+				Assert::Fail();
+			}
+		}
+	public:
+
+		TEST_METHOD(checkValidParams_getneg10_10_2_neg10_exceptionThrown)
+		{
+			double a = 10;
+			double b = -10;
+			double h = 2;
+			int n = -10;
+
+			try {
+				checkValidParams(a, b, h, n);
+				Assert::Fail();
+			}
+			catch (...)
+			{
+				Assert::IsTrue(true);
+			}
+		}
+	};
+}
